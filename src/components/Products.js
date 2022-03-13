27db2344911cb13771a6ffc615cb10cb062d74c2
William Siqueira <wsiqueira@rbi.com>
2022-03-13 00:25:55 -0300

    Style improvements

--- src/components/Products.js
+++ src/components/Products.js
@@ -5,22 +5,24 @@ import formatCurrency from '../utils/formatCurrency'
 const Products = props => {
   const { products, addToCart } = props
   return (
-    <ul className='products'>
-      {
-        products.map(product => {
-          return (
-            <li key={product.id}>
-              <img src={product.image} alt={product.title} />
-              <span className='title'>{product.title}</span>
-              <span className='price'>
-                <span>{formatCurrency(product.price)}</span>
-                <button onClick={() => { addToCart(product) }}>Add to cart</button>
-              </span>
-            </li>
-          )
-        })
-      }
-    </ul>
+    <div className='products'>
+      <ul>
+        {
+          products.map(product => {
+            return (
+              <li key={product.id}>
+                <img src={product.image} alt={product.title} />
+                <span className='title'>{product.title}</span>
+                <span className='price'>
+                  <span>{formatCurrency(product.price)}</span>
+                  <button onClick={() => { addToCart(product) }}>Add to cart</button>
+                </span>
+              </li>
+            )
+          })
+        }
+      </ul>
+    </div>
   )
 }
 
